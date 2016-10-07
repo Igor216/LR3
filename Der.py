21b5be81337c9f89c5615f2aa651d323b3bde70e
Igor216 <ignased@mail.ru>
2016-10-07 15:10:58 +0300

    Add files via upload

--- Der.py
+++ Der.py
@@ -1,7 +1,6 @@
 import Base
 from datetime import datetime
 import requests
-import requests.exceptions
 
 class DerivID (Base.BaseClient):
     params = None
@@ -13,13 +12,7 @@ class DerivID (Base.BaseClient):
         return '{0}/{1}'.format(self.BASE_URL, method)
 
     def _get_data(self, method, http_method):
-        try:
-            response = requests.get(self.generate_url(self.method), self.params)
-        except requests.RequestException:
-            print("Не удалось выполнить запрос!")
-            exit(0)
-
-
+        response = requests.get(self.generate_url(self.method), self.params)
         return self.response_handler(response)
 
     def find_id(self,ss):
@@ -47,11 +40,7 @@ class DerivFriends(Base.BaseClient):
         return '{0}/{1}'.format(self.BASE_URL, method)
 
     def _get_data(self, method, http_method):
-        try:
-            response = requests.get(self.generate_url(self.method), self.params)
-        except requests.RequestException:
-            print("Не удалось выполнить запрос!")
-            exit(0)
+        response = requests.get(self.generate_url(self.method), self.params)
 
         return self.response_handler(response)
 
@@ -59,23 +48,20 @@ class DerivFriends(Base.BaseClient):
         a = ss.json()
         dict_age = {0: 0}
 
-        try:
-            for bdate in a.get("response"):
-                date_str = str(bdate.get("bdate"))
-                if (not (bdate.get("bdate")) == None) and (len(date_str) > 7):
-                    dt = datetime.strptime(date_str, "%d.%m.%Y")
-                    delta_date = str(datetime.now() - dt)
-                    i = int(delta_date.split()[0])
-                    val = i // 365
-                    if dict_age.get(val) == None:
-                        dict_age[val] = '*'
-                    else:
-                        dict_age[val] = str(dict_age.get(val)) + '*'
+        for bdate in a.get("response"):
+            date_str = str(bdate.get("bdate"))
+            if (not (bdate.get("bdate")) == None) and (len(date_str) > 7):
+                dt = datetime.strptime(date_str, "%d.%m.%Y")
+                delta_date = str(datetime.now() - dt)
+                i = int(delta_date.split()[0])
+                val = i // 365
+                if dict_age.get(val) == None:
+                    dict_age[val] = '#'
                 else:
-                    dict_age[0] = int(dict_age.get(0)) + 1
-            self.print_gist(dict_age)
-        except TypeError:
-            print("Пользователь удален,невозможно построить гистограмму!")
+                    dict_age[val] = str(dict_age.get(val)) + '#'
+            else:
+                dict_age[0] = int(dict_age.get(0)) + 1
+        self.print_gist(dict_age)
 
     def print_gist(self, dict_age):
         print()
@@ -83,8 +69,8 @@ class DerivFriends(Base.BaseClient):
         for key in sorted(dict_age):
             if key == 0:
                 sum += int(dict_age[0])
-                print("Нет возраста: ", dict_age[0])
+                print("Не указан возраст: ", dict_age[0])
             else:
                 sum += int(len(str((dict_age[key]))))
                 print("%3d %s" % (key, dict_age[key]))
-        print("Всего друзей: ", sum)
+        print("Друзья: ", sum)
